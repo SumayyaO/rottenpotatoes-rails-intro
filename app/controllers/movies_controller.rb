18952d418f377987e8be9c70e59095f9ae18fcdf
EC2 Default User <ec2-user@ip-172-31-6-123.us-east-2.compute.internal>
2018-09-26 23:24:55 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
   def index
     sort_choice= params[:sort]
     if sort_choice == 'title'
-      @movie = Movie.order(:sort)
+      @movies = Movie.order(:title)
     end  
     if sort_choice == "release_date"
     #  @movies = Movie.all
